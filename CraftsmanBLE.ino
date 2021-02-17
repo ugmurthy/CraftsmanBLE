c1d600dbb2bd47990a84046571a7b31d2d62d0a1
U G Murthy <ugmurthy@gmail.com>
2021-02-17 12:04:03 -0800

    CraftsmanBLE 1.0

---
+++ CraftsmanBLE.ino
@@ -0,0 +1,310 @@
+////////////////////////////////////
+// Author: U G Murthy
+// Date : 16-FEB-2021
+// For : Kosha Designs
+// Project : Craftsman
+////////////////////////////////////
+/*
+ Video: https://www.youtube.com/watch?v=oCMOYS71NIU
+    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleNotify.cpp
+    Ported to Arduino ESP32 by Evandro Copercini
+ 
+*/
+// #include <M5StickC.h>
+// uncomment ^ here for M5StickC
+// comment next line for M5SticlC
+#include "M5StickCPlus.h"
+#include "EasyButton.h"
+
+// BLE Related
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+
+BLEServer *pServer = NULL;
+BLECharacteristic * pTxCharacteristic;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+char message[100];
+
+void dumpBLE(char* msg);
+
+// BLE Related 
+
+// find appropriate values for M5Stick for BUTTON A,B - see reverse
+// of the device for numbers
+#define BTN_A 37
+#define BTN_B 39
+
+
+EasyButton Button_A(BTN_A,40);
+EasyButton Button_B(BTN_B,40);
+
+
+int dest = 1;
+int delta_T = 100;
+
+// for future
+float threshold = 1.2;
+int scale = 10;
+bool start_read = false;
+
+float accX;
+float accY;
+float accZ;
+
+// readings buffer
+// 1+3 floats x 4 bytes x 4000 = 64k
+float acc_buff[16000];
+long idx;
+bool folded = false;
+long seqno = 0;
+
+// func declarations
+void acc_init(int dest);
+void acc_read(void);
+void Display_readings(float X,float Y,float Z);
+void on_B_Pressed();
+void on_A_Pressed();
+
+void acc_init(int dest) {
+  // find appropriate line for initialising IMU for M5StickC
+  M5.Imu.Init();
+  if (dest==1) {
+    Serial.println("AccX,AccY,AccZ");
+    }
+}
+
+
+void acc_read(int seq,int dest) {
+
+  //find appropriate line for reading X,Y,Z values from IMU
+  M5.Imu.getAccelData(&accX, &accY, &accZ);
+  
+  if (dest == 1) {
+    Serial.print(seq);
+    Serial.print(" ,");
+    Serial.print(accX);
+    Serial.print(" ,");
+    Serial.print(accY);
+    Serial.print(" ,");
+    Serial.println(accZ);
+    }
+}
+
+/// SCreen routines
+void Display_readings(float X,float Y,float Z,float seq){
+  M5.Lcd.setCursor(40,30);
+  M5.Lcd.printf("%7.2f  ",X );
+  M5.Lcd.setCursor(40,50);
+  M5.Lcd.printf("%7.2f  ",Y );
+  M5.Lcd.setCursor(40,70);
+  M5.Lcd.printf("%7.2f  ",Z );
+  M5.Lcd.setCursor(70,90);
+  M5.Lcd.printf("%04d",int(seq));
+}
+
+void on_A_Pressed() {
+  Serial.println("Button A has been pressed! Toggle read");
+  start_read = !start_read;
+}
+
+void on_B_Pressed() {
+  // DUMP Data
+  int seqno = 0;
+  start_read = false;
+  int upto=0;
+
+  // Assess how much of the buffer is full of readings
+  // check if we have folded
+  if (folded) {
+    upto = sizeof(acc_buff)/4-1;
+  } else {
+    upto = idx ;
+  }
+
+  // Get a header out including json header
+  sprintf(message,"{\"Device\":\"M5Craftsman\",\"period\":%d}\n",delta_T);
+  dumpBLE(message);
+  Serial.print(message);
+  
+  sprintf(message,"%s\n","SeqNo, AccX, AccY, AccZ");
+  dumpBLE(message);
+  Serial.print(message);
+
+  // DUMP the buffer
+  for (int i=0;i<upto;i=i+4) {
+    seqno = (int) acc_buff[i];
+    sprintf(message,"%04d, %5.2f, %5.2f, %5.2f\n",(int)seqno,acc_buff[i+1]*scale,acc_buff[i+2]*scale,acc_buff[i+3]*scale);
+    dumpBLE(message);
+    Serial.print(message);
+    // Lets not tax the BLE - give it some rest!
+    delay(10);  
+    }
+}
+// a9a778fe-c8d9-4f73-af1d-f4e407e9a78c
+// BLE related classes and call backs
+#define SERVICE_UUID           "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" // UART service UUID
+#define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
+#define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
+
+// on connect/disconnect callbacks
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+      Serial.println("BLE Connected");
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+      Serial.println("BLE Disconnected");
+    }
+};
+
+// for future - not used now.
+// for control purposes.
+class MyCallbacks: public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *pCharacteristic) {
+      std::string rxValue = pCharacteristic->getValue();
+      std::string subsRx;
+      
+      if (rxValue.length() > 0) {
+        
+        Serial.print("Received Value: ");
+        Serial.print(rxValue.c_str());
+        subsRx = rxValue.substr(0,4);
+        Serial.print(" Substring :");
+        Serial.println(subsRx.c_str());
+      }
+    }
+};
+// BLE related classes and call backs - ENDS here
+
+//
+void setup() {
+  
+  
+  Serial.begin(115200);
+  Button_A.begin();
+  Button_B.begin();
+
+  Button_A.onPressed(on_A_Pressed);
+  Button_B.onPressed(on_B_Pressed);
+  
+
+  M5.begin();
+  M5.Lcd.setRotation(3);
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setTextSize(2);
+  M5.Lcd.setCursor(10, 5);
+  M5.Lcd.println("Acc Readings");
+  M5.Lcd.setCursor(15,30);
+  M5.Lcd.println("X");
+  M5.Lcd.setCursor(15,50);
+  M5.Lcd.println("Y");
+  M5.Lcd.setCursor(15,70);
+  M5.Lcd.println("Z");
+  M5.Lcd.setCursor(15,90);
+  M5.Lcd.println("Seq");
+  Serial.print("Delta T     = ");
+  Serial.println(delta_T);
+  Serial.print("Threshold   = ");
+  Serial.println(threshold);
+  Serial.print("Destination = ");
+  Serial.println(dest);
+  Serial.print("BufferSz/4  = ");
+  Serial.println(sizeof(acc_buff)/4);
+  
+  // Initialise Acceleromenter 1 means serial port
+  acc_init(1);
+  // initialise buf_ptr
+
+
+  /// SETUP BLE SERVER
+  // Create the BLE Device
+  BLEDevice::init("Kosha CraftsMan");
+
+  // Create the BLE Server
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  // Create a BLE Characteristic
+  pTxCharacteristic = pService->createCharacteristic(
+                    CHARACTERISTIC_UUID_TX,
+                    BLECharacteristic::PROPERTY_NOTIFY
+                  );
+                      
+  pTxCharacteristic->addDescriptor(new BLE2902());
+
+  BLECharacteristic * pRxCharacteristic = pService->createCharacteristic(
+                       CHARACTERISTIC_UUID_RX,
+                      BLECharacteristic::PROPERTY_WRITE
+                    );
+
+  pRxCharacteristic->setCallbacks(new MyCallbacks());
+
+  // Start the service
+  pService->start();
+
+  // Start advertising
+  pServer->getAdvertising()->start();
+  Serial.println("Waiting for a client connection to send Acc data...");
+  
+  
+}
+
+/// SEND DATA via BLE
+void dumpBLE(char* msg) {
+  if (deviceConnected) {
+        pTxCharacteristic->setValue((uint8_t*)msg, strlen(msg));
+        pTxCharacteristic->notify();
+  }
+}
+
+void loop() {
+  
+  int buf_size = sizeof(acc_buff);
+  
+  Button_A.read();
+  Button_B.read();
+  
+  if (start_read) {
+    
+    acc_read(seqno,dest);
+    
+    acc_buff[idx]=float(seqno);
+    seqno++;
+    acc_buff[idx+1]=accX;
+    acc_buff[idx+2]=accY;
+    acc_buff[idx+3]=accZ;
+    
+    Display_readings(accX,accY,accZ,float(seqno));    
+    idx += 4; 
+
+    if (idx >= buf_size/4) {
+       // foldit 
+      folded = true;
+      idx = 0 ;
+    }  
+
+    delay(delta_T);
+  }
+
+  // disconnecting
+  if (!deviceConnected && oldDeviceConnected) {
+        delay(500); // give the bluetooth stack the chance to get things ready
+        pServer->startAdvertising(); // restart advertising
+        Serial.println("start advertising");
+        oldDeviceConnected = deviceConnected;
+  }
+  // connecting
+  if (deviceConnected && !oldDeviceConnected) {
+  // do stuff here on connecting
+      oldDeviceConnected = deviceConnected;
+  }
+  
+}
---
+++ README.md
@@ -0,0 +1,34 @@
+## CRAFTSMANBLE 
+
+CraftsMan BLE is designed to send accelerometer data over BLE and serial port toany BLE Client and/or computer with a serial port. 
+
+Systems requirements:
+1. M5 Stick-C Plus (the device that will send the data) as BLE Server
+2. BLE Client a Mobile phone using **Bluefruit LE Connect** on iOS or Android
+3. and a Laptop with Arduino IDE 
+
+### Installation
+1. Download the CraftsmanBLE.ino file to your Arduino sketches directory
+2. Compile it (Make necessary changes in case you are using a M5-Stick instead of a M5 Stick C plus. Note the only changes needed with be the Button numbers for Button A,B, Accelerometer initialisation and Accelerometer read commands, and the LCD print commands.
+
+### Usage
+1. Reset the M5-StickC plus
+2. Connect Mobile to "Kosha Craftsman" servie using the BLE software
+3. Hit Connect
+4. Click UART under modules section
+5. On the M5-StickC Plus - Click Button A to start reading accelerometer readings
+6. After a few seconds click Button A to stop the accelerometer readings.
+7. M5 Screen will show the number of readings taken(see last line)
+8. Click button B - to dump the readings in CSV format to BLE Client device
+9. Click "Settings" icon, Export, Copy as txt.
+
+### Libraries used
+1. M5 Stick C plus
+2. ESP32 Arduino BLE
+
+### Other info:
+1. CraftsmanBLE is set to take readings every 100 ms. you can change that by changing delta_T
+2. The current buffer size can accomodate a max of 4000 accelerometer readings, if the number of readings exceed 4000 then the buffer pointer will fold back to 0 and start writing from top of buffer. Acts like a ring buffer.
+3. Sorting the data on seqno will provide you the right order of readings even if the buffer has folded.
+4. a 10ms delay is inserted while writing to BLE client- this is ensure BLE server is not overwhelmed.
+
